fdce64246fdf1906f9b934e2402d9cb5862c86fd
Ward Figge <wfig26@gmail.com>
2015-06-26 20:52:37 -0700

    added pagination gem

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,7 +4,7 @@ before_action :set_pin, only: [:show, :edit, :update, :destroy]
 before_action :correct_user, only: [:edit, :update, :destroy]
 
   def index
-    @pins = Pin.all.order("created_at DESC")
+    @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 8)
   end
 
   def show
